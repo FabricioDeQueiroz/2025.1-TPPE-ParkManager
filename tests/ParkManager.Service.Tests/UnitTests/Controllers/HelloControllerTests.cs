f665580eb38d559d9275b5d3ba561aac72976daf
FabricioDeQueiroz <fabriciodequeiroz2016@gmail.com>
2025-05-09 10:07:16 -0300

    feat(tests): Adicionando testes unitários com xUnit.

---
+++ tests/ParkManager.Service.Tests/UnitTests/Controllers/HelloControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using ParkManager_Service.Controllers;
+
+namespace ParkManager.Service.Tests.UnitTests.Controllers;
+
+public class HelloControllerTests
+{
+    private readonly HelloController _controller = new();
+
+    [Fact(DisplayName = "Hello World")]
+    public void Get_ReturnsExpectedMessage()
+    {
+        var result = _controller.Get();
+        
+        Assert.Equal("Olá Mundo!", result);
+    }
+    
+    [Fact(DisplayName = "Ignorado 1", Skip = "Ignorado")]
+    public void Get_Ignorado()
+    {
+        // Implementar depois
+    }
+}
