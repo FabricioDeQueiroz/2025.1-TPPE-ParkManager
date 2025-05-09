dee7372cd1065bc2facc44c8c30eb4b2b04706cd
FabricioDeQueiroz <fabriciodequeiroz2016@gmail.com>
2025-05-09 09:59:45 -0300

    feat(backend): Criação do Backend .NET com endpoint Hello World.

---
+++ ParkManager-Service/Controllers/HelloController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ParkManager_Service.Controllers;
+
+[ApiController]
+[Route("/")]
+public class HelloController() : ControllerBase
+{
+    [HttpGet(Name = "GetHello")]
+    public string Get()
+    {
+        return "Olá Mundo!";
+    }
+}
