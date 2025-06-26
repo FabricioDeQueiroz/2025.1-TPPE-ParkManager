850cbe5b3dd1d8e24ea47553efe5892ae9d1ea45
FabricioDeQueiroz <fabriciodequeiroz2016@gmail.com>
2025-06-26 14:15:17 -0300

    refactor(infra): Refatorando Dockerfiles para utilizar entrypoint e aplicar migrations automaticamente.

---
+++ ParkManager-Service/entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+DB_HOST=${DB_HOST:-db}
+DB_PORT=${DB_PORT:-5432}
+
+MAX_RETRIES=30
+RETRY_COUNT=0
+
+echo "Esperando DB aceitar conexões..."
+until (echo > /dev/tcp/$DB_HOST/$DB_PORT) >/dev/null 2>&1 || [ $RETRY_COUNT -ge $MAX_RETRIES ]; do
+  echo "DB não disponível - dormindo por 2 segundos..."
+  sleep 2
+  RETRY_COUNT=$((RETRY_COUNT+1))
+done
+
+if [ $RETRY_COUNT -ge $MAX_RETRIES ]; then
+    echo "DB não ficou disponível a tempo. Saindo."
+    exit 1
+fi
+
+if [ "$ASPNETCORE_ENVIRONMENT" = "Development" ]; then
+  echo "DB está disponível. Aplicando migrations com Fakes para testes..."
+
+  dotnet ef database update --project ParkManager-Service.csproj
+elif [ "$ASPNETCORE_ENVIRONMENT" = "Production" ]; then
+  echo "DB está disponível. Aplicando migrations..."
+
+  dotnet ef database update SchemaMigration --project ParkManager-Service.csproj
+else
+  echo "ASPNETCORE_ENVIRONMENT não setado ou não definido. Aplicando migrations padrão..."
+  
+  dotnet ef database update SchemaMigration --project ParkManager-Service.csproj
+fi
+
+echo "Migrations aplicadas com sucesso. Iniciando a aplicação..."
+
+if [ "$ASPNETCORE_ENVIRONMENT" = "Development" ]; then
+  echo "Iniciando aplicação em modo de desenvolvimento com dotnet watch run..."
+
+  dotnet test ParkManager-Service.csproj -l 'console;verbosity=normal' --logger:trx
+  dotnet watch --project ParkManager-Service.csproj run --urls=http://0.0.0.0:8080
+elif [ "$ASPNETCORE_ENVIRONMENT" = "Production" ]; then
+  echo "Iniciando aplicação em modo de produção..."
+
+  dotnet ParkManager-Service.dll
+else
+  echo "ASPNETCORE_ENVIRONMENT não setado ou não definido. Iniciando aplicação em modo padrão..."
+  
+  dotnet ParkManager-Service.dll
+fi
